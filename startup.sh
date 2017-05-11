2a4e79a31bf0e353031b862cd86fc3555a24d32e
Leo Jääskeläinen <a1600576@myy.haaga-helia.fi>
2017-05-11 22:40:54 +0000

    Updated version

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo | sudo add-apt-repository ppa:webupd8team/brackets
-echo | sudo apt-get update
-echo | sudo apt-get install -y puppet
+sudo apt-get update
+sudo apt-get install -y puppet
+sudo cp -r ./puppetdigi /etc/puppet/modules
+echo | sudo puppet apply -e 'class {pakettimoduuli:}'
 
