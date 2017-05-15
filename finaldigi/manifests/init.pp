c81cd5c48122513c93293dbcc7229b5954c14d05
leksenderi <a1600576@myy.haaga-helia.fi>
2017-05-15 10:49:24 +0000

    Update init.pp

--- finaldigi/manifests/init.pp
+++ finaldigi/manifests/init.pp
@@ -10,4 +10,14 @@ class pakettimoduuli {
                 allowcdrom => 'true',
 	}
 	
+	exec{'bootstrap':
+                command => "/usr/bin/wget -q https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip",
+                creates => "/home/xubuntu/bootstrap",
+        }
+
+        file{'/home/xubuntu/bootstrap':
+                mode => 0755,
+                require => Exec["bootstrap"],
+        }
+
 }
