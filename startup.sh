6c6dbd4970066235f65c2710d8c98499f5809229
leksenderi <a1600576@myy.haaga-helia.fi>
2017-05-15 09:33:16 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ echo | sudo add-apt-repository ppa:webupd8team/brackets
 sudo apt-get update
 sudo apt-get install -y puppet
 sudo cp -r ./finaldigi /etc/puppet/modules/pakettimoduuli
-echo | sudo puppet apply -e 'class {pakettimoduuli:}'
+sudo puppet apply -e 'class {pakettimoduuli:}'
