188f0093e381b78c0d2bb0cc1c72e02b44d4aa35
leksenderi <a1600576@myy.haaga-helia.fi>
2017-05-16 22:24:27 +0000

    Update init.pp

--- finaldigi/manifests/init.pp
+++ finaldigi/manifests/init.pp
@@ -1,19 +1,12 @@
 class pakettimoduuli {
-        
-        exec {'apt-get update':
-                command => '/usr/bin/apt-get update',
-                refreshonly => true,
-                }
-
+          
         package { brackets:
                 ensure => 'installed',
-                require => Exec['apt-get update'],
                 allowcdrom => 'true',
         }
 
         package { apache2:
                 ensure => 'installed',
-                require => Exec['apt-get update'],
                 allowcdrom => 'true',
         }
 
