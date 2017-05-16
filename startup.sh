927aa56f3ef51dea389734a20daef448934c3b02
leksenderi <a1600576@myy.haaga-helia.fi>
2017-05-16 12:52:21 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,3 +5,4 @@ sudo apt-get install -y puppet
 sudo cp -r ./finaldigi /etc/puppet/modules/pakettimoduuli
 sudo puppet apply -e 'class {pakettimoduuli:}'
 sudo cp -r template.html /var/www/html/index.html
+firefox 'localhost'
