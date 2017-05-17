6fb1dc8eecd21b156f23a629c195e7f272594122
leksenderi <a1600576@myy.haaga-helia.fi>
2017-05-17 11:26:10 +0000

    Update init.pp

--- finaldigi/manifests/init.pp
+++ finaldigi/manifests/init.pp
@@ -9,9 +9,4 @@ class pakettimoduuli {
                 ensure => 'installed',
                 allowcdrom => 'true',
         }
-
-        exec { 'openBrackets':
-                command => '/usr/bin/brackets &',
-                require => Package['brackets'],
-        }
 }
